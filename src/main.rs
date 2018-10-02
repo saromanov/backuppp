c7a204f29dde1cbe9d48b558167d4b78f1e04229
Sergey <xxsmotur@gmail.com>
2018-10-02 22:23:09 +0500

    matchign of storages

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,16 @@ mod dump;
 
 use config::Config;
 
+fn backup_mongo(conf:Config){
+    match mongo::mongo_dump(conf) {
+       Ok(dr) => {
+           println!("Created archive file: {:}", dr.name)
+       }
+       Err(why) => {
+           println!("{:}?", why)
+       }
+   }
+}
 fn main() {
     let conf = Config{
         database:"".to_string(),
@@ -15,17 +25,12 @@ fn main() {
         backup_storage:"mongo".to_string(),
     };
 
-    match conf.backup_storage {
-        "mongo" => {
-            
+    match &conf.backup_storage.as_ref() {
+        &"mongo" => {
+            backup_mongo(conf);
+        }
+        _ => {
+            println!("unable to find backup storage")
         }
     }
-   match mongo::mongo_dump(conf) {
-       Ok(dr) => {
-           println!("Created archive file: {:}", dr.name)
-       }
-       Err(why) => {
-           println!("{:}?", why)
-       }
-   }
 }
