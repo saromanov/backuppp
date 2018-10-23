29ced63f529f334829c872f78100fa34a6114f3b
Sergey <xxsmotur@gmail.com>
2018-10-23 23:46:57 +0500

    Implementation of upload on s3

--- src/upload.rs
+++ src/upload.rs
@@ -1,3 +1,6 @@
+use s3::bucket::Bucket;
+use s3::credentials::Credentials;
+
 use std::process::Command;
 const GCE_UPLOAD_COMMAND: &str = "gsutil";
 
@@ -6,4 +9,11 @@ pub fn upload_to_gce(bucket:String, name:String){
     output.arg("cp").arg(format!("{}", name)).arg(format!("gs://{}/{}", bucket, name))
     .output()
     .expect("unable to execute command");
+}
+
+pub fn upload_to_aws(bucket:String, name:String){
+    let credentials = Credentials::default();
+    let region = REGION.parse().unwrap();
+    let bucket = Bucket::new(bucket, region, credentials);
+    let (_, code) = bucket.put("test_file", MESSAGE.as_bytes(), "text/plain").unwrap();
 }
